37c2d490e5efba730586c5851273b2be66086a65
root <root@DESKTOP-67TLRHA.localdomain>
2022-12-01 13:05:49 -0800

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ import sys
 
 
 if __name__ == "__main__":
-    exit()
 
     args = len(sys.argv)
     if (args <= 1):
-        print("{} arguments.".format(args - 1))
+        print("{} argument.".format(args - 1))
     elif (args == 2):
         print("{} arguments:".format(args - 1))
     else:
