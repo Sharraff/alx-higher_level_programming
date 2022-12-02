e2b62f94826a29981faee5c51e509a0ce45f84fb
root <root@DESKTOP-67TLRHA.localdomain>
2022-12-02 01:51:41 -0800

    checker please !

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = int(argv[1])
+    b = int(argv[3])
+    sign = argv[2]
+    lenght = len(argv) - 1
+    if (lenght != 3):
+        print("Usage:{:s} <a> <operator> <b>")
+        exit(1)
+    if sign == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sign == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sign == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sign == '/':
+        print("{:.0f} / {:.0f} = {:.0f}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and / ")
+        exit(1)
