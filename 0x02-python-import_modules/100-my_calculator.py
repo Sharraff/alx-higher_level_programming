50037075c2d5381c0992a98d8ab736cd83c5655c
root <root@DESKTOP-67TLRHA.localdomain>
2022-12-02 03:57:53 -0800

    quick fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
@@ -6,7 +8,7 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     sign = argv[2]
-    lenght = len(argv) - 1
+    lenght = len(argv)
     if (lenght != 3):
         print("Usage:./100-my_calculator.py <a> <operator> <b>")
         exit(1)
