9b9a350e1dec7d6cdaa15cd04e93bef535b3dab4
root <root@DESKTOP-67TLRHA.localdomain>
2023-03-16 14:46:22 -0700

    sql query

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,6 @@
--- state max temps
-SELECT state, MAX(value) AS max_temp FROM temperatures
-GROUP BY state ASC;
+-- Displays max states temperature
+-- DDL query that displays the max temperature of each state, stored by state
+SELECT state,MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
