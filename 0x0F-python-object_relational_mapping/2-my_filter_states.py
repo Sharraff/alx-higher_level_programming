a9d8440e5e8a6ad36c64c875d69a4e3c6c0d205c
root <root@DESKTOP-67TLRHA.localdomain>
2023-03-24 17:05:43 -0700

    quick fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 # gets all states via python yee boi with your own state
+import MySQLdb
+from sys import argv
 
-def main(args):
+
+if __name__ == "__main__":
     # gets all state stuff by N
     if len(args) != 5:
         raise Exception("need 4 arguments!")
     db = MySQLdb.connect(host='localhost',
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3])
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM WHERE name LIKE '{}' ORDER BY id ASC"
+        "SELECT * FROM WHERE name LIKE '{:s}' ORDER BY id ASC"
         .format(args[4]))
     states = cur.fetchall()
     for state in states:
-        if state[1] == args[4]:
+        if state[1] == argv4]:
             print(state)
-
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
-    main(sys.argv)
+    cur.close()
+    db.close()
