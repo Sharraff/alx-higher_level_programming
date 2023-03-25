12e8aeb4777ee586df0ba35ffd781f83ff74e8e1
root <root@DESKTOP-67TLRHA.localdomain>
2023-03-24 17:10:25 -0700

    a fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,16 +6,15 @@ from sys import argv
 
 if __name__ == "__main__":
     # gets all state stuff by N
-    if len(args) != 5:
-        raise Exception("need 4 arguments!")
     db = MySQLdb.connect(host='localhost',
                          user=argv[1],
+                         port=3306,
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
     cur.execute(
         "SELECT * FROM WHERE name LIKE '{:s}' ORDER BY id ASC"
-        .format(args[4]))
+        .format(argv[4]))
     states = cur.fetchall()
     for state in states:
         if state[1] == argv4]:
