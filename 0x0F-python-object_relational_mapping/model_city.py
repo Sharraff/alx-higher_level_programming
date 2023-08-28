683fc2c3874698e0da7fd22834dd0d9165c93358
Sharraff <rogandsteinporter@gmail.com>
2023-08-28 03:18:08 -0700

    new updates and added advanced task

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script contains the class definition of a City
+"""
+
+from model_state import State, Base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Creates a city object
+    """
+    __tablename__ = 'cities'
+
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey(
+        'states.id'), nullable=False)
